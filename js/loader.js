f35d26aaa456b7fc4bff2dde7a23e5875cd9c4df
linettemanuel <38947725+linettemanuel@users.noreply.github.com>
2019-04-15 07:42:48 +0200

    Add files via upload

---
+++ js/loader.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+    var preloader = document.querySelector(".preloader");
+    setTimeout(()=> {
+        preloader.style.animation = "fadeOut 1s ease forwards"
+        preloader.style.zIndex = -20;
+    },2000);
+}
+  
